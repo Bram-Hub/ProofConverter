68311700c529054356479592f5e044a10b2c58f3
risch1997 <risch1997@aol.com>
2018-04-27 12:50:34 -0400

    Modified Sentence constructor to properly parse sentences. May not be 100% correct

--- src/proofconverter/Sentence.java
+++ src/proofconverter/Sentence.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Sentence {
 	private String operator;
 	private List<Sentence> sentences;
-	private AtomicSentence atomicSentence;
+	private Sentence singleSentence;
 	private String type;
 	
 	public Sentence() {
@@ -21,36 +21,40 @@ public class Sentence {
 		if(s.charAt(0) == ('(')) {
 			s = s.substring(1, s.length() - 1);
 			this.operator = s.substring(0, s.indexOf(' '));
-			s = s.substring(s.indexOf(' ') + 1, s.length()) + " ";
+			s = s.substring(s.indexOf(' ') + 1, s.length());
 			
-			if(this.operator.equals("�")) {
+			if(this.operator.equals("¬")) {
 				sentences = null;
-				atomicSentence = new AtomicSentence(s);
+				singleSentence = new Sentence(s);
 			} else {
-				int lastSpace = -1;
 				for(int i = 0; i < s.length(); i++) {
-					if(s.charAt(i) == ' ') {
-						sentences.add(new Sentence(s.substring(lastSpace + 1, i)));
-						lastSpace = i;
-					} else if(s.charAt(i) == '(') {
-						String subSentence = s.substring(i, i + s.substring(i, s.length()).indexOf(')') + 1);
-						sentences.add(new Sentence(subSentence));
-						i = i + subSentence.indexOf(')') + 1;
+					if(s.charAt(i) == '(') {
+						sentences.add(new Sentence(s.substring(i, s.indexOf(')') + 1)));
+						s = s.substring(s.indexOf(')') + 1, s.length());
+						i = -1;
+					} else if(s.charAt(i) != ' ') {
+						if(s.indexOf(' ') == -1) {
+							sentences.add(new Sentence(s.substring(i, s.length())));
+						} else {
+							sentences.add(new Sentence(s.substring(i, s.indexOf(" "))));
+							s = s.substring(s.indexOf(' ') + 1, s.length());
+							i = -1;
+						}
 					}
 				}
 			}
 		} else {
 			operator = "";
 			sentences = null;
-			atomicSentence = new AtomicSentence(s);
+			singleSentence = new AtomicSentence(s);
 		}
 		
 		//Determine the type of sentence based on the operator
-		if(operator.equals("&")) {
+		if(operator.equals("∧")) {
 			type = "Conjunction";
-		} else if(operator.equals("|")) {
+		} else if(operator.equals("∨")) {
 			type = "Disjunction";
-		} else if(operator.equals("�")) {
+		} else if(operator.equals("¬")) {
 			type = "Negation";
 		} else if(operator.equals("->")) {
 			type = "Conditional";
@@ -61,15 +65,19 @@ public class Sentence {
 	
 	public String printSentence() {
 		if(type == "Atomic") {
-			return atomicSentence.printSentence();
+			return singleSentence.printSentence();
 		} else if(type == "Negation") {
-			return operator + atomicSentence.printSentence();
+			if(singleSentence.getType().equals("Atomic") || singleSentence.getType().equals("Negation")) {
+				return operator + singleSentence.printSentence();
+			} else {
+				return operator + "(" + singleSentence.printSentence() + ")";
+			}
 		} else {
 			String output = "";
 			for(int i = 0; i < sentences.size() - 1; i++) {
-				output += sentences.get(i).printSentence() + operator;
+				output += sentences.get(i).printSentence() + " " + operator;
 			}
-			output += sentences.get(sentences.size() - 1).printSentence();
+			output +=  " " + sentences.get(sentences.size() - 1).printSentence();
 			return output;
 		}
 	}
